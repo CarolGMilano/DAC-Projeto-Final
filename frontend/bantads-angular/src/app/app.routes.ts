431e5eeae50109b256282c850b8b48762d01ff94
Carolina Milano <carolinamilanorg@gmail.com>
2026-03-14 19:02:15 -0300

    Adição da rota de CRUD de gerentes e limpeza do arquivo HTML central.

--- frontend/bantads-angular/src/app/app.routes.ts
+++ frontend/bantads-angular/src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { Routes } from '@angular/router';
+import { Gerentes } from './pages';
 import { CustomerDashboard } from './components/customer-dashboard/customer-dashboard';
 
 export const routes: Routes = [
+    //Telas iniciais
+    //OBS.: Apenas deixei como Gerentes pois ainda não foram criadas as páginas.
+    { path: "", component: Gerentes },
+    { path: "cadastrar", component: Gerentes },
+    
+    //Painel "Cliente"
     {
         path:"costumerDashboard",
         component: CustomerDashboard
-    }
+    },
+
+    //Painel "Gerente"
+
+    //Painel "Administrador"
+    { path: "admin/gerentes", component: Gerentes }
 ];
