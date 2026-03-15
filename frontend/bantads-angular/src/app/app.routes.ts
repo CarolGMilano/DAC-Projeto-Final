87a42a9eb11eae10f8f3aefac992ef82dcd1d26b
viniciusabprr <risktds@gmail.com>
2026-03-15 19:58:08 -0300

    feat(ui) implement change-profile ui

--- frontend/bantads-angular/src/app/app.routes.ts
+++ frontend/bantads-angular/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Gerentes } from './pages'
+import { CustomerDashboard } from './components/customer-dashboard/customer-dashboard';
+import {ProfileChange} from './pages/cliente/profile-change/profile-change'
+export const routes: Routes = [
+    //Telas iniciais
+    //OBS.: Apenas deixei como Gerentes pois ainda não foram criadas as páginas.
+    { path: "", component: Gerentes },
+    { path: "cadastrar", component: Gerentes },
+    
+    //Painel "Cliente"
+    {
+        path:"custumerDashboard",
+        component: CustomerDashboard
+    },
 
-export const routes: Routes = [];
+   { 
+    path: 'profileChange', 
+    component: ProfileChange
+  },
+    //Painel "Gerente"
+
+    //Painel "Administrador"
+    { path: "admin/gerentes", component: Gerentes }
+];
