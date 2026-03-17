d3cecce0f5fd1b5bad7f861adeea6de8a9859e99
Leagueofcorno <cauadias.henrique@gmail.com>
2026-03-17 14:46:48 -0300

    R13-Buscar-Cliente

--- frontend/bantads-angular/src/app/app.routes.ts
+++ frontend/bantads-angular/src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Gerentes } from './pages';
 import { CustomerDashboard } from './components/customer-dashboard/customer-dashboard';
 import { ProfileChange } from './pages/cliente/profile-change/profile-change';
 import { BankStatementLookup } from './components/bank-statement-lookup/bank-statement-lookup';
-
+import { ClientSearch } from './components/client-search/client-search'; 
+import { ClientList } from './components/client-list/client-list'
 export const routes: Routes = [
     // Telas iniciais
     { path: "", component: Gerentes },
@@ -26,5 +27,15 @@ export const routes: Routes = [
 
     {
         path: "bankStatement", component: BankStatementLookup
-    }
+    },
+
+    // R12: Lista de clientes
+    {
+        path: "clientList", component: ClientList
+    },
+    
+    // R13: Buscar cliente específico
+    {
+        path: "client-search", component: ClientSearch
+    },
 ];
---
+++ frontend/bantads-angular/src/app/components/client-list/client-list.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-list',
+  imports: [],
+  templateUrl: './client-list.html',
+  styleUrl: './client-list.css',
+})
+//R12: Consultar Todos os Clientes - Deve apresentar em uma tabela todos os seus clientes, contendo CPF, Nome, Cidade, Estado, Saldo da conta, Limite da conta. Deve ser ordenado de forma crescente por Nome. Deve ser disponibilizado um campo de texto onde o gerente pode pesquisar o cliente por CPF (ou parte dele) e Nome (ou parte dele). Cada cliente deve possuir um link que, ao ser pressionado, vai para uma tela contendo todos os dados do cliente e de sua conta;
+
+export class ClientList {
+
+}
