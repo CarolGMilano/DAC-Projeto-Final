a757ca94a310e208d2fcbdddc93dee9126869773
VitorHigen <vitorhigen@ufpr.br>
2026-03-26 01:46:31 -0300

    correcao relatorio + manager dashboard feito

--- frontend/bantads-angular/src/app/components/manager-dashboard/manager-dashboard.ts
+++ frontend/bantads-angular/src/app/components/manager-dashboard/manager-dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared';
+import { MoedaBrPipe } from '../../shared/pipes/moedaBr/moeda-br-pipe';
 
 @Component({
   selector: 'app-manager-dashboard',
-  imports: [],
+  imports: [SharedModule,MoedaBrPipe],
   templateUrl: './manager-dashboard.html',
   styleUrl: './manager-dashboard.css',
 })
@@ -15,15 +17,21 @@ e salário do cliente, junto com dois botões "Aprovar" e "Recusar".
 
 export class ManagerDashboard {
 
-exemplos: { cpf: number; name: string; salario: number }[] = [
-  { cpf: 1, name: 'John', salario: 10 },
-  { cpf: 2, name: 'Sarah', salario: 20 },
-  { cpf: 3, name: 'Sarah', salario: 20 }
+exemplos: { id:number, cpf: string; nome: string; salario: number }[] = [
+  { id: 1, cpf: "12345678909", nome: "Carlos Eduardo da Silva", salario: 3500 },
+  { id: 2,cpf: "98765432100", nome: "Mariana Oliveira Santos", salario: 7200 },
+  { id: 3,cpf: "45678912366", nome: "João Pedro Almeida Costa", salario: 2800 },
+  { id: 4,cpf: "74185296311", nome: "Fernanda Souza Ribeiro", salario: 5100 },
+  { id: 5,cpf: "15935725844", nome: "Lucas Henrique Martins", salario: 6400 },
+  { id: 6,cpf: "85245615977", nome: "Patrícia Gomes Ferreira", salario: 4300 },
+  { id: 7,cpf: "36925814722", nome: "Rafael Teixeira Barbosa", salario: 8900 }
 ];
 
-recusarAutoCadastro(Cliente: any) {
+recusar(id:number) {
+ this.exemplos = this.exemplos.filter(c => c.id !== id);
 }
-aprovarAutoCadastro(Cliente: any) {
+aprovar(id: number) {
+ this.exemplos = this.exemplos.filter(c => c.id !== id);
 }
 
 }
