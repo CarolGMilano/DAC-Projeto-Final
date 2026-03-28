47387871e827f8a89d56b69c1887db79ebdf06cd
Carolina Milano <carolinamilanorg@gmail.com>
2026-03-28 12:50:00 -0300

    Atualização dos imports das páginas

--- frontend/bantads-angular/src/app/app.routes.ts
+++ frontend/bantads-angular/src/app/app.routes.ts
@@ -1,17 +1,14 @@
 import { Routes } from '@angular/router';
 
-import { Login, Cadastro, Gerentes } from './pages';
+import { 
+    Login, Cadastro, 
+    CustomerDashboard, ProfileChange, BankStatementLookup,
+    ManagerDashboard, ClientList, ClientSearch, Top3Clients,
+    AdministratorDashboard, Gerentes, RelatorioDeClientes
+} from './pages';
+
 import { MainLayout } from './layout';
 
-import { CustomerDashboard } from './pages/cliente/customer-dashboard/customer-dashboard';
-import { ProfileChange } from './pages/cliente/profile-change/profile-change';
-import { BankStatementLookup } from './components/bank-statement-lookup/bank-statement-lookup';
-import { ClientSearch } from './components/client-search/client-search'; 
-import { ClientList } from './components/client-list/client-list'
-import { AdministratorDashboard } from './components/administrator-dashboard/administrator-dashboard';
-import { ManagerDashboard } from './components/manager-dashboard/manager-dashboard';
-import { RelatorioDeClientes } from './pages/relatorio-de-clientes/relatorio-de-clientes';
-import { Top3Clients } from './components/top3-clients/top3-clients';
 export const routes: Routes = [
     //Telas iniciais
     { path: "", component: Login },
@@ -27,7 +24,7 @@ export const routes: Routes = [
             { path: "cliente/depositar", component: CustomerDashboard },
             { path: "cliente/sacar", component: CustomerDashboard },
             { path: "cliente/transferir", component: CustomerDashboard },
-            { path: "cliente/extrato", component: CustomerDashboard },
+            { path: "cliente/extrato", component: BankStatementLookup },
 
             //Painel "Gerente"
             { path: "gerente", component: ManagerDashboard },
@@ -38,9 +35,7 @@ export const routes: Routes = [
             //Painel "Administrador"
             { path: "admin", component: AdministratorDashboard },
             { path: "admin/gerentes", component: Gerentes },
-            { path: "admin/relatorio", component: RelatorioDeClientes },
-
-            { path: "bankStatement", component: BankStatementLookup },
+            { path: "admin/relatorio", component: RelatorioDeClientes }
         ]
     }
 ];
