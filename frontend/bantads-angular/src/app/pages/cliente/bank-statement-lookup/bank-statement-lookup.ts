57f03e90eb4289a80528b2583e6112f23566d335
Diinoue <diinouebr@gmail.com>
2026-03-29 17:52:11 -0300

    Feito

--- frontend/bantads-angular/src/app/pages/cliente/bank-statement-lookup/bank-statement-lookup.ts
+++ frontend/bantads-angular/src/app/pages/cliente/bank-statement-lookup/bank-statement-lookup.ts
@@ -13,38 +13,62 @@ Anotações:
 */
 
 import { Component, OnInit } from '@angular/core';
+import { MoedaBrPipe } from '../../../shared/pipes/moedaBr/moeda-br-pipe';
 
 @Component({
   selector: 'app-bank-statement-lookup',
-  imports: [],
+  imports: [MoedaBrPipe],
   templateUrl: './bank-statement-lookup.html',
   styleUrl: './bank-statement-lookup.css',
 })
 
 
 export class BankStatementLookup implements OnInit{
+
+  // Período estabelecido para mostrar as transações;
   date = new Date("2026-02-01T00:00:00");
   date2 = new Date("2026-02-28T23:59:00");
 
+  // Saldo Inicial fictício
+  saldoInicial: number = 3289;
+
+  // Transações de exemplo;
     transacoesExemplo = [
-    {id: 0, data: "02/02/2026", hora: "12:00", operacao: "Transferência", clienteOrigem: "JOÃO EUCLIDES DA CUNHA", clienteDestino: "MÁRIO QUINTANA", valor: "30,00 R$"},
-    {id: 1, data: "08/02/2026", hora: "09:23", operacao: "Saque", clienteOrigem: "", clienteDestino: "", valor: "200,00 R$"},
-    {id: 2, data: "09/02/2026", hora: "10:43", operacao: "Depósito", clienteOrigem: "", clienteDestino: "", valor: "500,00 R$"},
-    {id: 3, data: "11/02/2026", hora: "01:20", operacao: "Transferência", clienteOrigem: "DALTON TREVISAN", clienteDestino: "JOÃO EUCLIDES DA CUNHA", valor: "1212,24 R$"},
-    {id: 4, data: "14/02/2026", hora: "01:20", operacao: "Transferência", clienteOrigem: "DALTON TREVISAN", clienteDestino: "JOÃO EUCLIDES DA CUNHA", valor: "1212,24 R$"},
+    {id: 0, data: "02/02/2026", hora: "12:00", operacao: "Transferência", clienteOrigem: "JOÃO EUCLIDES DA CUNHA", clienteDestino: "MÁRIO QUINTANA", valor: -30, saldoNoMomento: 0},
+    {id: 1, data: "08/02/2026", hora: "09:23", operacao: "Saque", clienteOrigem: "", clienteDestino: "", valor: -200, saldoNoMomento: 0},
+    {id: 2, data: "09/02/2026", hora: "10:43", operacao: "Depósito", clienteOrigem: "", clienteDestino: "", valor: 500, saldoNoMomento: 0},
+    {id: 3, data: "11/02/2026", hora: "01:20", operacao: "Transferência", clienteOrigem: "DALTON TREVISAN", clienteDestino: "JOÃO EUCLIDES DA CUNHA", valor: 1212, saldoNoMomento: 0},
+    {id: 4, data: "14/02/2026", hora: "01:20", operacao: "Transferência", clienteOrigem: "DALTON TREVISAN", clienteDestino: "JOÃO EUCLIDES DA CUNHA", valor: 500, saldoNoMomento: 0},
+    {id: 5, data: "14/02/2026", hora: "02:20", operacao: "Transferência", clienteOrigem: "DALTON TREVISAN", clienteDestino: "JOÃO EUCLIDES DA CUNHA", valor: 200, saldoNoMomento: 0},
+    {id: 6, data: "21/02/2026", hora: "15:16", operacao: "Saque", clienteOrigem: "", clienteDestino: "", valor: -800, saldoNoMomento: 0},
   ]
-  // Todas as transações de tal período
+  // TRANSACTIONS ARRAY: Vetor contendo TODAS as transações de tal período, independente de página
   transactionsArray: any[] = [];
 
+  // Variáveis relacionadas a página
+    // Página atual
+    pagAtual: number = 0;
+    // Máx transações p/pag
+    tamPag: number = 15;
+  
+  // Inicializa o TRANSACTIONS ARRAY para ter todas as transações de tal periodo;
   ngOnInit(){
-    this.transactionsArray = this.popularTransacoes(this.date, this.date2);
+    this.transactionsArray = this.popularTransacoes(this.date, this.date2, this.saldoInicial);
   }
 
-  popularTransacoes(start: Date, end: Date): any[] {
-    const result: any[] = [];
-    const current = new Date(start);
+  // 1.FUNÇÕES
+    // 1.1 POPULAR TRANSAÇÕES: Itera por cada dia, se uma do transacoesExemplo bater o dia, ele é colocado dentro;
+    
+    /* O formato da população funciona da seguinte forma:
+        // O vetor result é um vetor de objetos (87), contendo a data e um outro vetor de objetos, que é a transacoesDoDia:
+          // transacoesDoDia pega as transacoes que batem com o dia com filter() e coloca no vetor result, em seu respectivo dia;
+    */
 
-    while (current <= end ) {
+  popularTransacoes(start: Date, end: Date, balance: number): any[] {
+    const result: any[] = []; // Vai ser o transactionsArray quando obter todas as transações do período
+    const current = new Date(start); // Acompanha a data durante a iteração do while;
+
+    while (current <= end ) { // Para cada dia, obtem todas as transações no dia e coloca 
       const dataLocal = current.toLocaleDateString('pt-BR');
       // console.log(`dataLocal: ${dataLocal}, ${this.transacoesExemplo[0].data}`);
       
@@ -52,6 +76,17 @@ export class BankStatementLookup implements OnInit{
       // Filtra transações que pertencem a esse dia;
       const transacoesDoDia = this.transacoesExemplo.filter(t => t.data === dataLocal);
 
+      // Coloca o saldo do dia
+      transacoesDoDia.forEach((e) => {
+        this.saldoInicial += e.valor
+        e.saldoNoMomento = this.saldoInicial;
+        console.log(`saldoInicial após mudança no valor: ${this.saldoInicial}`);
+        console.log(`saldoNoMomento após receber saldoInicial: ${e.saldoNoMomento}`);
+      }
+      );
+
+      // Se tiver transações, pega as transações filtradas
+        // Caso contrário, cria uma transação vazia
       const transacoesPush = transacoesDoDia.length > 0 ?
       transacoesDoDia :
       [{
@@ -61,23 +96,47 @@ export class BankStatementLookup implements OnInit{
         operacao: "",
         clienteOrigem: "",
         clienteDestino: "",
-        valor: "0"
+        valor: "0",
+        saldoNoMomento: this.saldoInicial
       }]
+
+      // Coloca no vetor result a data e as transações
       const indexNew = result.push({
           data: dataLocal,
           transacoes: transacoesPush
       });
 
       // Consolog teste
-      console.log(result[indexNew - 1]);
+      // console.log(result[indexNew - 1]);
 
-      // Próximo dia
+      // Move para o próximo dia
       current.setDate(current.getDate() + 1);
     }
     return result;
   }
 
-  // Consolog teste
+  // Paginação
+    get paginatedTransactions(): any[] {
+    const start = this.pagAtual * this.tamPag;
+    const end = start + this.tamPag;
+    return this.transactionsArray.slice(start, end);
+  }
+  
+    // Próxima página
+  proximaPagina() {
+    if((this.pagAtual + 1) * this.tamPag < this.transactionsArray.length) {
+      this.pagAtual++;
+    } else {
+      console.log("proximaPagina: deu ruim");      
+    }
+  }
+    // Página anterior
+  paginaAnterior(){
+    if(this.pagAtual > 0) {
+      this.pagAtual--;
+    }
+  }
+  
 
 
 }
