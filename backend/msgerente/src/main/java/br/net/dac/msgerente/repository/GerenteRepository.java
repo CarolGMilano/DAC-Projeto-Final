61e2536c930a76895561a353aa16c6c7d738b210
Carolina Milano <carolinamilanorg@gmail.com>
2026-04-19 20:48:49 -0300

    Criação do repository do msgerente

---
+++ backend/msgerente/src/main/java/br/net/dac/msgerente/repository/GerenteRepository.java
@@ -0,0 +1,12 @@
+package br.net.dac.msgerente.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.*;
+import br.net.dac.msgerente.model.Gerente;
+
+public interface GerenteRepository extends JpaRepository<Gerente, Long>{
+  public Gerente findByCpf(String cpf);
+  List<Gerente> findByAtivoTrue();
+  Gerente findByCpfAndAtivoTrue(String cpf);
+}
