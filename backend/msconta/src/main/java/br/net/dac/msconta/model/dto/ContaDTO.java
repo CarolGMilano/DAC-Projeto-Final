e53fc6a7b36aa09dd7fab31f893baa339eb2a41b
VitorHigen <vitorhigen@ufpr.br>
2026-04-21 14:05:46 -0300

    metodo na service de comando

--- backend/msconta/src/main/java/br/net/dac/msconta/model/dto/ContaDTO.java
+++ backend/msconta/src/main/java/br/net/dac/msconta/model/dto/ContaDTO.java
@@ -3,6 +3,7 @@ package br.net.dac.msconta.model.dto;
 import java.sql.Date;
 
 public class ContaDTO {
+    private boolean ativo;
     private Long id;
     private Long idGerente;
     private Long idCliente;
@@ -14,8 +15,11 @@ public class ContaDTO {
     public ContaDTO() {
     }
 
-    public ContaDTO(long id, String numeroConta, Date dataCriacao, double saldo, double limite) {
+    public ContaDTO(boolean ativo, Long id, Long idGerente, Long idCliente, String numeroConta, Date dataCriacao, double saldo, double limite) {
+        this.ativo = ativo;
         this.id = id;
+        this.idGerente = idGerente;
+        this.idCliente = idCliente;
         this.numeroConta = numeroConta;
         this.dataCriacao = dataCriacao;
         this.saldo = saldo;
@@ -24,24 +28,68 @@ public class ContaDTO {
 
     // Getters e Setters
 
-    public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
+    public boolean isAtivo() {
+        return ativo;
+    }
 
-    public Long getIdGerente() {return idGerente;}
-    public void setIdGerente(Long idGerente) {this.idGerente = idGerente;}
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
 
-    public Long getIdCliente() {return idCliente;}
-    public void setIdCliente(Long idCliente) {this.idCliente = idCliente;}
+    public Long getId() {
+        return id;
+    }
 
-    public String getNumeroConta() {return numeroConta;}
-    public void setNumeroConta(String numeroConta) {this.numeroConta = numeroConta;}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public Date getDataCriacao() {return dataCriacao;}
-    public void setDataCriacao(Date dataCriacao) {this.dataCriacao = dataCriacao;}
+    public Long getIdGerente() {
+        return idGerente;
+    }
 
-    public Double getSaldo() {return saldo;}
-    public void setSaldo(Double saldo) {this.saldo = saldo;}
+    public void setIdGerente(Long idGerente) {
+        this.idGerente = idGerente;
+    }
 
-    public Double getLimite() {return limite;}
-    public void setLimite(Double limite) {this.limite = limite;}
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
 }
