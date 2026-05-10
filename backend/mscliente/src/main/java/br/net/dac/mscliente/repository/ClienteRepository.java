1e9352b554206b301d0a9682153b8f2533c64330
Vinicius de Abreu <risktds@gmail.com>
2026-05-10 10:23:29 -0300

    feat: add client controller and service prototype

---
+++ backend/mscliente/src/main/java/br/net/dac/mscliente/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package br.net.dac.mscliente.repository;
+
+import br.net.dac.mscliente.model.entity.ClienteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+    Optional<ClienteEntity> findByCpf(String cpf);
+    boolean existsByCpf(String cpf);
+    Optional <ClienteEntity> findByIdUsuario(long idUsuario);
+}
