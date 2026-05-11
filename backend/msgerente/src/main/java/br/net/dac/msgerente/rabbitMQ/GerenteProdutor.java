f73b7280a141322e84566533b9788fa6281ab6e1
Carolina Milano <carolinamilanorg@gmail.com>
2026-05-10 21:35:06 -0300

    Alterações de configuração do RabbitMQ para integração com a SAGA

--- backend/msgerente/src/main/java/br/net/dac/msgerente/rabbitMQ/GerenteProdutor.java
+++ backend/msgerente/src/main/java/br/net/dac/msgerente/rabbitMQ/GerenteProdutor.java
@@ -12,28 +12,15 @@ public class GerenteProdutor {
   @Autowired
   private RabbitTemplate rabbitTemplate;
 
-  //Nome da exchange (mesma que foi configurado)
-  private static final String EXCHANGE = "bantads.exchange";
+  public static final String CRIACAO_SUCESSO = "msgerente.criacao.sucesso";
+  public static final String CRIACAO_FALHA = "msgerente.criacao.falha";
 
-  //Routing keys de SAÍDA (eventos que esse MS vai publicar)
-  private static final String GERENTE_CRIACAO_SUCESSO = "MSGERENTE.CRIACAO.SUCESSO";
-  private static final String GERENTE_CRIACAO_FALHA = "MSGERENTE.CRIACAO.FALHA";
-
-  //private static final String GERENTE_ALTERACAO_SUCESSO = "MSGERENTE.ALTERACAO.SUCESSO";
-  //private static final String GERENTE_ALTERACAO_FALHA = "MSGERENTE.ALTERACAO.FALHA";
-  
-  //private static final String GERENTE_DELECAO_SUCESSO = "MSGERENTE.DELECAO.SUCESSO";
-  //private static final String GERENTE_DELECAO_FALHA = "MSGERENTE.DELECAO.FALHA";
-
-  //Evento de sucesso
+  //Criar objeto genérico
   public void criacaoSucesso(GerenteCriacaoSucessoEvent evento) {
-    System.out.println("Enviando GERENTE_CRIACAO_SUCESSO" + evento);
-    rabbitTemplate.convertAndSend(EXCHANGE, GERENTE_CRIACAO_SUCESSO, evento);
+    rabbitTemplate.convertAndSend(CRIACAO_SUCESSO, evento);
   }
 
-  //Evento de erro
   public void criacaoFalha(GerenteCriacaoFalhaEvent erro) {
-    System.out.println("Enviando GERENTE_CRIACAO_FALHA -> " + erro.getMensagem());
-    rabbitTemplate.convertAndSend(EXCHANGE, GERENTE_CRIACAO_FALHA, erro);
+    rabbitTemplate.convertAndSend(CRIACAO_FALHA, erro);
   }
 }
