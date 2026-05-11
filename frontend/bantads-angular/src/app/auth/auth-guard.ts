252735979f4d87edcf2c586e466fba43499aac46
Leagueofcorno <cauadias.henrique@gmail.com>
2026-05-11 06:07:07 -0300

    microsserviço funcionando,mas sem integração com o banco

--- frontend/bantads-angular/src/app/auth/auth-guard.ts
+++ frontend/bantads-angular/src/app/auth/auth-guard.ts
@@ -3,41 +3,37 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { AuthService } from '../auth/auth-service';
 import { TipoUsuario } from '../shared/models/EnumTipoUsuario';
 
-@Injectable
-({
+@Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate
-{
+export class AuthGuard implements CanActivate {
 
-  constructor
-  (
+  constructor(
     private router: Router,
     private authService: AuthService
   ) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean
-  {
-
-    if (!this.authService.isAutenticado())
-    {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    if (!this.authService.isAutenticado()) {
       this.redirecionarParaLogin(state.url);
       return false;
     }
 
     const rolesPermitidas: TipoUsuario[] = route.data['role'] || [];
 
-    if (rolesPermitidas.length > 0 && !this.authService.temPermissao(rolesPermitidas))
-    {
-      this.redirecionarParaLogin();
+    if (rolesPermitidas.length > 0 && !this.authService.temPermissao(rolesPermitidas)) {
+      this.redirecionarParaAcessoNegado();
       return false;
     }
 
     return true;
   }
 
-  private redirecionarParaLogin(urlDestino?: string): void
-  {
-    this.router.navigate(['/login'],{queryParams: { redirectTo: urlDestino }});
+  private redirecionarParaLogin(urlDestino?: string): void {
+    this.router.navigate(['/login'], { queryParams: { redirectTo: urlDestino } });
+  }
+
+  private redirecionarParaAcessoNegado(): void {
+    this.router.navigate(['/acesso-negado']);
   }
 }
