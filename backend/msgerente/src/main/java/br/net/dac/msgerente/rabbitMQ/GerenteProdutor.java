644e2382813223ad8ca10005edd1d61d8a8d9340
Carolina Milano <carolinamilanorg@gmail.com>
2026-05-17 19:34:26 -0300

    Atualização das estruturas do RabbitMQ do msgerente

--- backend/msgerente/src/main/java/br/net/dac/msgerente/rabbitMQ/GerenteProdutor.java
+++ backend/msgerente/src/main/java/br/net/dac/msgerente/rabbitMQ/GerenteProdutor.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import br.net.dac.msgerente.model.event.Evento;
 import br.net.dac.msgerente.model.event.GerenteCriacaoFalhaEvent;
 import br.net.dac.msgerente.model.event.GerenteCriacaoSucessoEvent;
 
@@ -12,15 +13,24 @@ public class GerenteProdutor {
   @Autowired
   private RabbitTemplate rabbitTemplate;
 
-  public static final String CRIACAO_SUCESSO = "msgerente.criacao.sucesso";
-  public static final String CRIACAO_FALHA = "msgerente.criacao.falha";
+  public static final String CRIACAO_SUCESSO = "msgerente.queue.sucesso";
+  public static final String CRIACAO_FALHA = "msgerente.queue.falha";
 
-  //Criar objeto genérico
-  public void criacaoSucesso(GerenteCriacaoSucessoEvent evento) {
+  public void criacaoSucesso(GerenteCriacaoSucessoEvent payload) {
+    Evento evento = new Evento();
+
+    evento.setTipo("CRIAR_GERENTE_SUCESSO");
+    evento.setPayload(payload);
+
     rabbitTemplate.convertAndSend(CRIACAO_SUCESSO, evento);
   }
 
   public void criacaoFalha(GerenteCriacaoFalhaEvent erro) {
-    rabbitTemplate.convertAndSend(CRIACAO_FALHA, erro);
+    Evento evento = new Evento();
+
+    evento.setTipo("CRIAR_GERENTE_FALHA");
+    evento.setPayload(erro);
+
+    rabbitTemplate.convertAndSend(CRIACAO_FALHA, evento);
   }
 }
