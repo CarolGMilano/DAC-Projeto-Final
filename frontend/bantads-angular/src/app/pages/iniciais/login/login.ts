76600d17071f56f8ea8e9231ec45ec443342fea8
Carolina Milano <carolinamilanorg@gmail.com>
2026-05-17 19:19:49 -0300

    Atualização dos componentes login e autocadastro

--- frontend/bantads-angular/src/app/pages/iniciais/login/login.ts
+++ frontend/bantads-angular/src/app/pages/iniciais/login/login.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 import { LoginService } from '../../../services';
-import { ILogin, TipoUsuario } from '../../../shared';
+import { ILogin, TipoUsuario, UsuarioStatus } from '../../../shared';
 
 @Component({
   selector: 'app-login',
@@ -16,11 +16,42 @@ export class Login {
   @ViewChild('formLogin') formLogin! : NgForm;
 
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
   readonly loginService = inject(LoginService);
-
+  
   login: ILogin = {
-    email: '',
+    login: '',
     senha: ''
+  }
+  
+  usuarioStatus: UsuarioStatus = UsuarioStatus.Nenhum;
+  status = UsuarioStatus;
+
+  loading: boolean = false;
+  mensagem!: string;
+
+  ngOnInit(): void {
+    const usuario = this.loginService.usuarioLogado;
+
+    if(usuario) {
+      const rotasPorTipo = {
+        [TipoUsuario.CLIENTE]: '/cliente',
+        [TipoUsuario.GERENTE]: '/gerente',
+        [TipoUsuario.ADMIN]: '/admin'
+      };
+
+      const rota = rotasPorTipo[usuario.tipo];
+    
+      this.router.navigate([rota]);
+    } else {
+      this.route.queryParams.subscribe(params => {
+        this.mensagem = params['error'];
+      })
+    }
+  }
+
+  aoMudarInput() {
+    this.usuarioStatus = UsuarioStatus.Nenhum;
   }
 
   mostrarSenha: boolean = true;
@@ -28,20 +59,33 @@ export class Login {
   logar() {
     if (!this.formLogin.form.valid) return;
 
-    const usuario = this.loginService.login(this.login.email, this.login.senha);
+    this.loginService.login(this.login).subscribe({
+      next: (usuario) => {
+        if(usuario != null) {
+          this.loginService.usuarioLogado = usuario;
 
-    if (usuario) {
-      if (usuario.tipo === TipoUsuario.CLIENTE) {
-        this.router.navigate(['/customerDashboard']);
-      } else if (usuario.tipo === TipoUsuario.GERENTE) {
-        this.router.navigate(['/gerente']);
-      } else if (usuario.tipo === TipoUsuario.ADMINISTRADOR) {
-        this.router.navigate(['/admin']);
+          if (usuario.tipo === TipoUsuario.CLIENTE) {
+            this.router.navigate(['/customerDashboard']);
+          } else if (usuario.tipo === TipoUsuario.GERENTE) {
+            this.router.navigate(['/gerente']);
+          } else if (usuario.tipo === TipoUsuario.ADMIN) {
+            this.router.navigate(['/admin']);
+          }
+        }
+      },
+      error: (erro) => {
+
+        if (erro.status === 401 || erro.status === 404) {
+          this.mensagem = erro.error;
+          this.usuarioStatus = UsuarioStatus.Invalido;
+        } else if (erro.status === 500) {
+          this.mensagem = 'Erro interno no servidor.';
+          this.usuarioStatus = UsuarioStatus.Invalido;
+        } else {
+          this.mensagem = 'Erro inesperado ao efetuar login.';
+          this.usuarioStatus = UsuarioStatus.Invalido;
+        }
       }
-
-      console.log("Usuário logado");
-    } else {
-      console.log("Usuário não existe")
-    }
+    });
   }
 }
